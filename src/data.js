006c94ee4024d44094334e5ed21253fdb35e4395
praneeth reddy <nakkapraneeth23899@gmail.com>
2022-11-12 20:24:02 -0500

    added files-initial commit

---
+++ src/data.js
@@ -0,0 +1,138 @@
+const data = [
+ 
+
+  {
+    question: "The production of items in a company is minimum and this stage is known as ?",
+    incorrectAnswers: [
+      "Recovery",
+      "Prosperity",
+      "Recession",
+    ],
+    correctAnswer: "Depression",
+  },
+  {
+    question: " An increase in prices over a festival is an example of ?",
+    incorrectAnswers: ["Secular trend", "Cyclic trend", "Irregular trend"],
+    correctAnswer: "Seasonal trend",
+  },
+
+  {
+    question: "In time series the mostly widely used mathematical technique to calculate the trend is ?",
+    incorrectAnswers: [
+      "Semi average",
+      "Moving average",
+      "Free hand curve",
+    ],
+    correctAnswer: "Least squares",
+  },
+
+  {
+    question: "In Time series the seasonal variations are ?",
+    incorrectAnswers: [
+      "Sudden variation",
+      "Long term variation",
+      "No varitation",
+    ],
+    correctAnswer: "Short term variation",
+  },
+  {
+    question: "What are the components of time series?",
+    incorrectAnswers: [
+      "Cyclic variation",
+      "Trend",
+      "Seasonal variation",
+    ],
+    correctAnswer: "All the options",
+  },
+
+  {
+    question: " If a Time series data changes at a constant increase/decrease then ",
+    incorrectAnswers: [
+      "A linear fit to the logarithms data gives a perfect fit",
+      "A nonlinear is required to fit and gives the perfect fit",
+      "A well-fitted trend line cannot be obtained",
+    ],
+    correctAnswer: "A linear line fitted to the data gives a perfect fit",
+  },
+{
+  question: "In Time series if a pattern of variation that occurs every year is known as ?",
+    incorrectAnswers: [
+      "Cyclical variation",
+      "Trend",
+      "Cyclic variation",
+    ],
+    correctAnswer: "Seasonal variation",
+  },
+
+  {
+    question: "Times series graphs are mostly ?",
+      incorrectAnswers: [
+        "Trend",
+        "Box plot",
+        "Line graph",
+      ],
+      correctAnswer: "Histogram",
+    },
+    {
+      question: " A bookstore company delaying the manufaction of books for some weeks is an example of ?",
+        incorrectAnswers: [
+          "Cyclic trend",
+          "Secular trend",
+          "Seasonal trend",
+        ],
+        correctAnswer: "Irregular trend",
+      },
+
+      {
+        question: "Choose one of the cross validation techniques which is better suited for time series data ?",
+          incorrectAnswers: [
+            "Stratified shuffle split cross validation",
+            "Leave one out cross validation",
+            "k-fold cross validation",
+          ],
+          correctAnswer: "Forward chaining cross validation",
+        },
+
+        {
+          question: "Scatter plots were drawn during correlation to check assumptions of ?",
+            incorrectAnswers: [
+              "Unbiasness",
+              "Multicollinearity",
+              "Homoscedasticity",
+            ],
+            correctAnswer: "Linearity",
+          },
+
+          {
+            question: "The trend in a time series which is over a long term appears as ?",
+              incorrectAnswers: [
+                "Straight-line",
+                "Downward",
+                "Upward",
+              ],
+              correctAnswer: "Parabolic curve or Third Degree Curve",
+            },
+
+            {
+              question: "We call a trend to be a better fitted trend if the sum of squares of residuals is :?",
+                incorrectAnswers: [
+                  "Maximum",
+                  "Negative",
+                  "Positive",
+                ],
+                correctAnswer: "Minimum",
+              },
+
+              {
+                question: " An accidental situation or a mal function in a company is an example of?",
+                  incorrectAnswers: [
+                    "Seasonal trend",
+                    "Cyclical variations",
+                    "Seasonal movements",
+                  ],
+                  correctAnswer: "Irregular variations",
+                },
+      
+];
+
+export default data;
